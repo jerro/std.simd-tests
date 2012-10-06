aa68b418c795922ffe80a49b7f07101c7c88ca3e
Jernej Krempuš <jkrempus@gmail.com>
2012-10-06 20:06:09 +0200

    More tests.

--- test.d
+++ test.d
@@ -2,12 +2,13 @@ import std.stdio, std.traits, std.math, std.conv, std.typetuple;
 import std.simd;
 import core.simd;
 
-template staticIota(size_t n, r...)
+template staticIota(int start, int end, int stride = 1)
 {
-    static if(n == 0)
-        alias r staticIota;
-    else 
-        alias staticIota!(n - 1, n - 1, r) staticIota;
+    static if(start >= end)
+        alias TypeTuple!() staticIota;
+    else
+        alias TypeTuple!(start, staticIota!(start + stride, end, stride)) 
+            staticIota;
 }
 
 template concatenate(a...)
@@ -16,6 +17,14 @@ template concatenate(a...)
         enum concatenate = "";
     else 
         enum concatenate = to!string(a[0]) ~ concatenate!(a[1 .. $]);
+}
+
+template staticSelect(bool first, a...) if (a.length == 2)
+{
+    static if(first)
+        alias a[0] staticSelect;
+    else
+        alias a[1] staticSelect; 
 }
 
 template toFloatTemplate(alias a)
@@ -28,15 +37,17 @@ template toUByte(alias a)
     enum toUByte = cast(ubyte) a;
 }
 
-auto vector(A...)(A a)
+auto vectorT(T, A...)(A a)
 {
-    alias Vector!(A[0][A.length]) V;
+    alias Vector!(T[A.length]) V;
     V r;
     foreach(i,_; A)
-        (cast(A[0]*) &r)[i] = cast(A[0]) a[i];
+        (cast(T*) &r)[i] = cast(T) a[i];
     
     return r;
 }
+
+auto vector(A...)(A a) { return vectorT!(A[0])(a); }
 
 auto equal(T)(T a, T b) if(isIntegral!T)
 {
@@ -52,7 +63,7 @@ auto equal(V)(V a, V b) if(!isFloatingPoint!V && !isIntegral!V)
 {
     alias typeof(a.array[0]) T;
 
-    foreach(i; staticIota!(V.init.length))
+    foreach(i; staticIota!(0, V.init.length))
         if(!equal(a.array[i], b.array[i]))
             return false;
 
@@ -137,7 +148,7 @@ void main()
     
     test!((_)
     {
-        foreach(i; staticIota!256)
+        foreach(i; staticIota!(0, 256))
         {
             alias TypeTuple!(i & 3, (i >> 2) & 3, (i >> 4) & 3, (i >> 6) & 3) e;
             auto v = vector(0f, 1, 2, 3);
@@ -148,7 +159,7 @@ void main()
 
     test!((_)
     {
-        auto v = vector(staticMap!(toUByte, staticIota!16));
+        auto v = vector(staticMap!(toUByte, staticIota!(0, 16)));
         auto mask = vector(
             cast(ubyte) 10, 4, 0, 1, 2, 11, 3, 6, 7, 8, 2, 9, 12, 6, 14, 15); 
 
@@ -203,5 +214,87 @@ void main()
     {
         assert(equal(toDouble(vector(1, 2, 3, 4)), vector(1.0, 2))); 
         assert(equal(toDouble(vector(1f, 2, 3, 4)), vector(1.0, 2))); 
+    });
+
+    test!((_)
+    {
+        auto v = vector(1 << 3, 2 << 3, 3 << 3, 4 << 3);
+        assert(equal(shiftRightImmediate!3(v), vector(1, 2, 3, 4))); 
+    });
+    
+    test!((_)
+    {
+        foreach(T; TypeTuple!(int4, float4))
+        {
+            auto v1 = cast(T) vector(1, 2, 3, 4);
+            auto v2 = cast(T) vector(1 | 8, 2 | 8, 3 | 8, 4 | 8);
+            auto v3 = cast(T) vector(8, 8, 8, 8);
+            assert(equal(xor(v1, v2), v3));
+        }
+    });
+
+    test!((_)
+    {
+        foreach(T; TypeTuple!(int, float))
+        {
+            auto v1 = vectorT!T(1, 2, 3, 4);
+            auto v2 = vectorT!T(5, 6, 7, 8);
+            auto mask = cast(void16) vector(0, -1, 0, -1);
+            assert(equal(std.simd.select(mask, v1, v2), vectorT!T(1, 6, 3, 8)));
+        } 
+    });
+
+    test!((_)
+    {
+        auto v1 = vector(1f, 2, 3, 4);
+        auto v2 = vector(2f, 2, 2, 2);
+        assert(equal(cast(int4) maskGreater(v1, v2), vector(0, 0, -1, -1)));
+    });
+
+    test!((_)
+    {
+        auto f32 = vector(1f, -2, 3, -4);
+        assert(equal(abs(f32), vector(1f, 2, 3, 4))); 
+        
+        auto i32 = vector(1, -2, 3, -4);
+        assert(equal(abs(i32), vector(1, 2, 3, 4))); 
+        
+        auto f64 = vector(1.0, -2);
+        assert(equal(abs(f64), vector(1.0, 2)));
+    });
+
+    test!((_)
+    {
+        foreach(T; TypeTuple!(byte, ubyte, short, ushort))
+        foreach(saturate; TypeTuple!(true, false))
+        foreach(addition; TypeTuple!(true, false))
+        static if(std.traits.isSigned!T || addition)
+        {
+            enum s = T.max;
+            enum n = 16 / T.sizeof;        
+            auto v1 = vectorT!T(staticIota!(1, 1 + n));
+            auto r = vectorT!T(staticIota!(2, 2 + 2 * n, 2));
+
+            static if(saturate)
+            { 
+                typeof(v1) y = cast(T)(11 + s / 2);
+                v1 = setY(v1, y);
+                y = cast(T)s; 
+                r = setY(r, y);
+            }
+
+            static if(addition)
+            {
+                alias staticSelect!(saturate, addSaturate, add) op;
+                auto v2 = v1;
+            }
+            else
+            {
+                alias staticSelect!(saturate, subSaturate, sub) op;
+                auto v2 = -v1;
+            }
+            
+            assert(equal(op(v1, v2), r));
+        }
     });
 }
