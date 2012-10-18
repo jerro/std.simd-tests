b5aa3f6e4b0c7a1493c4364d527e74b7bb9f6472
Jernej Krempuš <jkrempus@gmail.com>
2012-10-18 02:35:09 +0200

    Added sizes.d

---
+++ sizes.d
@@ -0,0 +1,27 @@
+import std.stdio, std.process, std.string, std.range, std.algorithm, std.conv;
+
+void main()
+{
+    bool[string] table;
+    foreach(l; std.algorithm.splitter(shell("./test -l")))
+        if(!l.canFind("test"))
+            table[l] = true;
+
+    struct S{ string name; size_t size; }
+    S[] arr;
+    
+    foreach(l; std.algorithm.splitter(shell("nm -St decimal test"), "\n"))
+    {
+        auto s = split(l);
+        if(s.empty)
+            continue;
+
+        auto name = s.back;
+        if(name in table)
+            arr ~= S(name, to!size_t(s[1]));
+    }
+
+    sort!"a.size < b.size"(arr);
+    foreach(e; arr)
+        writefln("%s %s", e.tupleof);
+}
